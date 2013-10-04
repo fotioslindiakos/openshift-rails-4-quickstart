dbe1b06ed13d4c62de370ac15af0f0b5295bdf6f
Fotios Lindiakos <fotios@redhat.com>
2013-10-04 12:21:35 -0400

    Created default Rails 4 app

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OpenshiftRails4Quickstart::Application.config.session_store :cookie_store, key: '_openshift-rails-4-quickstart_session'
